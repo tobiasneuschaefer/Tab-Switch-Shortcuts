2ee56c880ccb66aed11a55e74c7a7d6b616613f9
Tobias Neuschäfer <49942350+tobiasneuschaefer@users.noreply.github.com>
2025-11-05 12:38:48 +0100

    Add minor performance improvements
    Performance is improved if target tab is in not collapsed group and if next tab after a collapsed group is not in a group

--- src/background.js
+++ src/background.js
@@ -17,24 +17,39 @@ async function focusRelativeTab(direction) {
   const tabs = await browser.tabs.query({ currentWindow: true });
 
   // The next tab to the left or right of the active tab.
-  const targetTab = tabs[activeTab.index + direction];
+  let targetTab = tabs[activeTab.index + direction];
 
-  // If the target tab is not in a group
+  // If the target tab exists and is not in a group
   if (targetTab && targetTab.groupId === -1) {
     await browser.tabs.update(targetTab.id, { active: true });
     return;
   }
-  // If the target tab is in a group
-  else if (targetTab && targetTab.groupId !== -1) {
+  // If the target tab exists, it must be in a group
+  else if (targetTab) {
     // Get all collapsed tab groups
     const collapsedGroups = await browser.tabGroups.query({ collapsed: true });
     const collapsedGroupIds = new Set(collapsedGroups.map((group) => group.id));
 
+    // If the target tab's group is not collapsed, activate it
+    if (!collapsedGroupIds.has(targetTab.groupId)) {
+      await browser.tabs.update(targetTab.id, { active: true });
+      return;
+    }
+
     // Search for the next tab that is not in a collapsed group
     const end = direction > 0 ? tabs.length : -1;
-    for (let i = activeTab.index + direction; i !== end; i += direction) {
-      if (!collapsedGroupIds.has(tabs[i].groupId)) {
-        await browser.tabs.update(tabs[i].id, { active: true });
+    for (
+      let i = activeTab.index + direction + direction;
+      i !== end;
+      i += direction
+    ) {
+      targetTab = tabs[i];
+      // If new target tab is not in a group or its group is not collapsed, activate it
+      if (
+        targetTab.groupId === -1 ||
+        !collapsedGroupIds.has(targetTab.groupId)
+      ) {
+        await browser.tabs.update(targetTab.id, { active: true });
         return;
       }
     }
